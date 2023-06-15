22c90fc0147c6497af489d8ab785dd40d330a284
DanTower <godspowerdaniel72@gmail.com>
2023-06-15 13:58:26 +0100

    updated

---
+++ HomePage.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById('hamburger');
+const close = document.getElementById('close');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () =>{
+    navUL.classList.toggle('show');
+    hamburger.classList.toggle('hide');
+});
+
+
+close.addEventListener('click', () =>{
+    navUL.classList.toggle('show');
+    hamburger.classList.toggle('hide');
+});
